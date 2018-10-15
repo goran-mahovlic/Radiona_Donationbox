73ec262e6285bb9e1c994fb77df25021d10e5cb2
Goran Mahovlic <goran.mahovlic@gmail.com>
2018-10-15 21:41:23 +0200

    Adding README

--- donatioBoxV3/donatioBoxV3.ino
+++ donatioBoxV3/donatioBoxV3.ino
@@ -9,13 +9,6 @@
  Description: DonationBoxV3 box for presentation and exibitions - users can donate money or likes
  
  Based on Heltec esp32 wifi-lora-32 board
- 
- [x] Left button is to get random project
- [x]Right button is to Like project
- [ ] If you insert money it will be noted  to selected project
- [ ] Sends changes(in money and likes) to TTN network over LoRa
- [ ] sounds, and 
- [ ] game play if money is inserted...
 
  Version: 1.03 
 
@@ -29,36 +22,29 @@
 
 #include "inc/includes.h"
 
-//here you define how many project you have
+// Here you define how many project you have
 #define NO_PROJECTS 4
      
-    void setup() {
-      //interrupt every 100ms to check pins
-      setupTimers();
-
-      // Setup pins
-      setupAllPins();
-      
-      Serial.begin(115200);
-      Serial.println("Started");
-
-      // init LCD
-      setupLCD();
-      
-      // LMIC init
-      os_init();
-      // setupLoRa
-      setupLoRa();
-
-      // Start check for project change - change picture - send serial 
-      do_check(&checkjob);
-      
-      // Start LoRa send job (sending automatically starts OTAA too)
-      do_send(&sendjob);
-
-    }
-     
-    void loop() {
-      os_runloop_once();
+void setup() {
+  // Interrupt every 100ms to check for changes
+  setupTimers();
+  // Setup pins
+  setupAllPins();
+  // Start serial
+  Serial.begin(115200);
+  // Init LCD
+  setupLCD();
+  // LMIC init
+  os_init();
+  // SetupLoRa
+  setupLoRa();
+  // Start check for project change - change picture - send serial 
+  do_check(&checkjob);
+  // Start LoRa send job (sending automatically starts OTAA too)
+  do_send(&sendjob);
+}
+ 
+void loop() {
+  os_runloop_once();
 }
 
